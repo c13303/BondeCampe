6d7649a44201f9c8af336de4d0b53087f7d41e72
c13303 <erreure@gmail.com>
2018-10-24 18:20:47 +0200

    config style

--- config/config-exemple.php
+++ config/config-exemple.php
@@ -2,3 +2,19 @@
 
 /* copy this file to config/config.php */
 
+
+$artistname = "Artist Name";
+$author = 'Site Author';
+$authorUrl = 'http://charlestorris.5tfu.org';
+$email = 'webmaster@zoobsystems.com';
+
+/* base site */
+$baseurl = 'http://URL_OF_THIS_SITE';
+
+
+/* default site share image */
+$ogimage = $baseurl.'/config/default.jpg';
+
+
+/* github url */
+$url = 'https://github.com/c13303/BondeCampe';
