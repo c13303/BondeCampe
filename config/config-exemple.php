14987580008c216670893ccca32a8b79b0c7b3f2
charles <erreure@gmail.com>
2025-12-14 12:04:04 +0000

    save

---
+++ config/config-exemple.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * chown -r user:www-data *
+ * chmod 775 archive
+ */
+/* copy this file to config/config.php */
+
+
+$artistname = "Artist Name";
+$author = 'Site Author';
+$authorUrl = 'http://charlestorris.5tfu.org';
+$email = 'webmaster@zoobsystems.com';
+
+/* base site */
+$baseurl = 'http://URL_OF_THIS_SITE';
+
+
+/* default site share image */
+$ogimage = $baseurl.'/config/default.jpg';
+
+
+/* github url */
+$url = 'https://github.com/c13303/BondeCampe';
+
+$background;
+$color;
+$linkcolor;
